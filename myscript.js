0d366a5ccd9833b1dd3f48482ec06c88b5ddcf44
Regina Jackson <rjackson156@gmail.com>
2022-03-27 21:26:13 -0400

    change click to mouseover

--- myscript.js
+++ myscript.js
@@ -1,10 +1,10 @@
-window.addEventListener('click', function(e) {
+window.addEventListener('mouseover', function(e) {
 
     const box = document.querySelector('div')
     box.classList.add('change')
 
     function removeChange() {
-        box.classList.remove('change')
+      box.classList.remove('change')
     }
 
     this.window.setTimeout(removeChange, 200)
